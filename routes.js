d44ed18daec60d8aafd688b2cb569ef629ea666b
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-04-14 21:51:52 +0530

    model

--- routes.js
+++ routes.js
@@ -5,4 +5,10 @@ const home = require('./controllers/home');
 const notice = require('./controllers/notice');
 router.get('/', home.home);
 router.get('/notice',notice.notice);
+router.post('/notice', (req,res) =>{
+    const title = req.body.title;
+    const message = req.body.message;
+    
+    res.end()
+})
 module.exports = router;
